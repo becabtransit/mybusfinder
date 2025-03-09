67dcaa5756d7f612f18476daadc7beee1a6367f8
BechirABIDI <starilax.echanges@gmail.com>
2025-03-09 19:22:21 +0000

    Mar 9, 2025, 8:22 PM

--- zestbus/proxy-cors/proxy_vehpos.php
+++ zestbus/proxy-cors/proxy_vehpos.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-vehicle-position';
+$url = 'https://proxy.transport.data.gouv.fr/resource/zest-menton-riviera-gtfs-rt-vehicle-position';
 $data = file_get_contents($url);
 echo $data;
 ?>
