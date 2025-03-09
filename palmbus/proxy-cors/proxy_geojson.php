3e9707b20e516aeb7bfa258b079b7909c2bfeb8c
BechirABIDI <starilax.echanges@gmail.com>
2025-03-09 00:39:43 +0000

    Mar 9, 2025, 1:39 AM

--- palmbus/proxy-cors/proxy_geojson.php
+++ palmbus/proxy-cors/proxy_geojson.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://transport.data.gouv.fr/resources/conversions/79560/GeoJSON';
+$url = 'https://transport.data.gouv.fr/resources/conversions/79420/GeoJSON';
 $data = file_get_contents($url);
 echo $data;
 ?>
