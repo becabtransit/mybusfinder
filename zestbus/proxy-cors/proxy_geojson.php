109da4d1bd388830231bb16973605149d5ca0cf7
BechirABIDI <starilax.echanges@gmail.com>
2025-03-09 19:21:56 +0000

    Mar 9, 2025, 8:21 PM

--- zestbus/proxy-cors/proxy_geojson.php
+++ zestbus/proxy-cors/proxy_geojson.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://transport.data.gouv.fr/resources/conversions/79420/GeoJSON';
+$url = 'https://transport.data.gouv.fr/resources/conversions/79559/GeoJSON';
 $data = file_get_contents($url);
 echo $data;
 ?>
