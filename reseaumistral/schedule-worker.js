17d3652586c5c467354c5e5fb8ea82ec26477133
BechirABIDI <starilax.echanges@gmail.com>
2025-03-21 20:50:39 +0000

    Mar 21, 2025, 9:50 PM

--- reseaumistral/schedule-worker.js
+++ reseaumistral/schedule-worker.js
@@ -1,55 +1,17 @@
-self.onmessage = function(e) {
-    const { routeId, stopId, destinationId, dateString, trips, stopTimes, calendar, calendarDates } = e.data;
+self.addEventListener('message', function(e) {
+    const { hour, minutes } = e.data;
     
-    const selectedDate = new Date(dateString);
-    const schedule = calculateSchedule(
-        trips,
-        stopTimes,
-        stopId,
-        destinationId,
-        selectedDate,
-        calendar,
-        calendarDates
-    );
-    
-    self.postMessage({
-        schedule,
-        formattedDate: selectedDate.toLocaleDateString('fr-FR', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        })
-    });
-};
+    const formattedMinutes = minutes
+        .sort((a, b) => a - b)
+        .map(min => min.toString().padStart(2, '0'))
+        .join('&nbsp;&nbsp;&nbsp;');
 
-function calculateSchedule(trips, stopTimes, stopId, destinationId, date, calendar, calendarDates) {
-    const activeServiceIds = getActiveServiceIds(date.getDay(), formatDate(date));
-    const schedule = [];
-    
-    // Optimisation : pré-filtrer les trips valides
-    const validTrips = trips.filter(trip => activeServiceIds.includes(trip.service_id));
-    
-    // Utiliser un Set pour les heures uniques
-    const uniqueHours = new Set();
-    
-    validTrips.forEach(trip => {
-        const tripStops = stopTimes[trip.trip_id];
-        if (!tripStops) return;
-        
-        let destinationReached = false;
-        
-        for (const stopTime of tripStops) {
-            if (stopTime.stop_id === destinationId) {
-                destinationReached = true;
-                break;
-            }
-            if (stopTime.stop_id === stopId && !destinationReached) {
-                uniqueHours.add(stopTime.arrival_time);
-                break;
-            }
-        }
-    });
-    
-    return Array.from(uniqueHours).sort();
-}
+    const html = `
+        <div class="schedule-row" data-hour="${hour}">
+            <span class="hour-label">${hour}h</span>
+            <span class="minutes-container">${formattedMinutes}</span>
+        </div>
+    `;
+
+    self.postMessage({ html });
+});
