ab718f3a7d1cf0d815e71e58b2a6ec9b2d785d26
BechirABIDI <starilax.echanges@gmail.com>
2025-03-24 12:43:37 +0000

    Mar 24, 2025, 1:43 PM

--- reseaumistral/proxy-cors/proxy_vehpos.php
+++ reseaumistral/proxy-cors/proxy_vehpos.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = '';
+$url = 'https://feed-rdtpm-toulon.ratpdev.com/VehiclePosition/GTFS-RT';
 $data = file_get_contents($url);
 echo $data;
 ?>
