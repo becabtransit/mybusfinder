584a6467414153bbea654483e09bb35976faa308
BechirABIDI <starilax.echanges@gmail.com>
2025-04-08 20:20:28 +0000

    Apr 8, 2025, 10:20 PM

--- tedbus/sw.js
+++ tedbus/sw.js
@@ -1,52 +1,29 @@
-const CACHE_VERSION = 'v1-' + new Date().getTime();
-const OFFLINE_CACHE = 'offline-only-' + CACHE_VERSION;
-const OFFLINE_PAGE = 'offline.html';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  
-  event.waitUntil(
-    caches.open(OFFLINE_CACHE).then(cache => {
-      return cache.add(OFFLINE_PAGE);
-    })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== OFFLINE_CACHE) {
-          return caches.delete(key);
-        }
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
 self.addEventListener('fetch', event => {
-  if (event.request.url.includes('/api/') || 
-      !event.request.url.startsWith(self.location.origin)) {
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request, { cache: 'no-store' })
-      .catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match(OFFLINE_PAGE);
-        }
-        
-        return new Response('', {
-          status: 503,
-          statusText: 'Service temporairement indisponible'
-        });
-      })
-  );
-});
-
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+    if (event.request.url.includes('/api/') || 
+        !event.request.url.startsWith(self.location.origin)) {
+      return;
+    }
+  
+    event.respondWith(
+      fetch(event.request, { cache: 'no-store' })
+        .catch(() => {
+          if (event.request.mode === 'navigate') {
+            console.log('Hors ligne offline.html');
+            return caches.match(OFFLINE_PAGE)
+              .then(response => {
+                if (response) {
+                  return response;
+                }
+                return new Response('Hors ligne et pas offline html.', {
+                  headers: { 'Content-Type': 'text/html' }
+                });
+              });
+          }
+          
+          return new Response('', {
+            status: 503,
+            statusText: 'Service temporairement indisponible'
+          });
+        })
+    );
+  });
