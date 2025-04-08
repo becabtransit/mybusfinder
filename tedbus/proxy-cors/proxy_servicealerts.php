7c5bc75a013dc4b06037b9659bec98b67060d494
BechirABIDI <starilax.echanges@gmail.com>
2025-04-08 16:34:40 +0000

    Apr 8, 2025, 6:34 PM

--- tedbus/proxy-cors/proxy_servicealerts.php
+++ tedbus/proxy-cors/proxy_servicealerts.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-service-alert';
+$url = 'https://pysae.com/api/v2/groups/draguignan/gtfs-rt';
 $data = file_get_contents($url);
 echo $data;
 ?>
