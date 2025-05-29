4957b92f674eb22199fac300e28496a023aa8b9b
BechirABIDI <starilax.echanges@gmail.com>
2025-05-29 23:24:19 +0000

    May 30, 2025, 1:24 AM

--- tan-nimes/proxy-cors/proxy_tripupdate.php
+++ tan-nimes/proxy-cors/proxy_tripupdate.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-trip-update';
+$url = 'https://transport.data.gouv.fr/resources/80731/download';
 $data = file_get_contents($url);
 echo $data;
 ?>
