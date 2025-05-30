6639f36ad846eaa4a010af8440e0d6e85f4acae5
BechirABIDI <starilax.echanges@gmail.com>
2025-05-30 17:40:31 +0000

    May 30, 2025, 7:40 PM

--- mta-ny/proxy-cors/proxy_servicealerts.php
+++ mta-ny/proxy-cors/proxy_servicealerts.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-$url = 'https://proxy.transport.data.gouv.fr/resource/palmbus-cannes-gtfs-rt-service-alert';
+$url = 'https://gtfsrt.prod.obanyc.com/alerts?key=95f7d9d0-ffb6-41dd-93ac-c79b6baa7db6';
 $data = file_get_contents($url);
 echo $data;
 ?>
