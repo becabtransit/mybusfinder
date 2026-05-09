f53538d0d779b8c6c9dc9bb3fb63ec6177779118
BechirABIDI <starilax.echanges@gmail.com>
2026-05-10 01:54:17 +0200

    ^^

--- app/networks/proxy-geocode.php
+++ app/networks/proxy-geocode.php
@@ -7,6 +7,6 @@ $lon = floatval($_GET['lon'] ?? 0);
 if (!$lat || !$lon) { echo '{}'; exit; }
 
 $url = "https://nominatim.openstreetmap.org/reverse?lat={$lat}&lon={$lon}&format=json&zoom=14&accept-language=fr";
-$ctx = stream_context_create(['http' => ['header' => "User-Agent: MyBusSchedule/1.0\r\n", 'timeout' => 5]]);
+$ctx = stream_context_create(['http' => ['header' => "User-Agent: MyBusFinder/1.0\r\n", 'timeout' => 5]]);
 $result = @file_get_contents($url, false, $ctx);
 echo $result ?: '{}';
